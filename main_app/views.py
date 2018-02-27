11d6fa9a448c157b03508be5a4a86a6b523934e8
James Anderson <james.p.anderson16@gmail.com>
2018-02-27 12:08:09 -0500

    adds show/detail view

--- main_app/views.py
+++ main_app/views.py
@@ -5,16 +5,6 @@ def index(request):
     treasures = Treasure.objects.all()
     return render(request, 'index.html', {'treasures': treasures})
 
-#
-# class Treasure:
-#     def __init__(self, name, value, material, location):
-#         self.name = name
-#         self.value = value
-#         self.material = material
-#         self.location = location
-#
-# treasures = [
-#     Treasure('Gold Nugget', 500.00, 'gold', "Curly's Creek, NM"),
-#     Treasure(name="Fool's Gold", value=0.00, material='pyrite', location="Fool's Falls, CO"),
-#     Treasure(name='Coffee Can', value=20.00, material='tin', location="Acme, CA")
-# ]
+def detail(request, treasure_id):
+    treasure = Treasure.objects.get(id=treasure_id)
+    return render(request, 'detail.html', {'treasure': treasure})
